1f6915623f2d7a2cb93a012c0e95eac2a48780a2
NQK923 <117274353+NQK923@users.noreply.github.com>
2024-10-07 06:03:55 +0700

    update list view

--- FrontEnd/frontend.client/src/app/view/ViewClient/list-view/list-view.component.ts
+++ FrontEnd/frontend.client/src/app/view/ViewClient/list-view/list-view.component.ts
@@ -1,5 +1,24 @@
 import {Component, OnInit} from '@angular/core';
+import {combineLatest, forkJoin, map} from "rxjs";
+import {Router} from "@angular/router";
+import {MangaService} from "../../../service/Manga/get_manga.service";
+import {ChapterService} from "../../../service/Chapter/get_chapter.service";
+import {BannerService} from "../../../service/Banner/banner.service";
 
+
+interface Manga {
+  id_manga: number;
+  name: string;
+  author: string;
+  num_of_chapter: number;
+  rating: number;
+  id_account: number;
+  is_posted: boolean;
+  cover_img: string;
+  describe: string;
+  updated_at: Date;
+  totalViews: number
+}
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
@@ -8,19 +27,36 @@ import {Component, OnInit} from '@angular/core';
 
 export class ListViewComponent implements OnInit {
   selectedValue: string = '';
+  mangas: Manga[] = [];
 
-  constructor() {
+  constructor(private router: Router, private mangaService: MangaService, private chapterService: ChapterService) {
   }
 
   ngOnInit(): void {
+    this.mangaService.getMangas().subscribe(mangas => {
+      this.mangas = mangas;
+      const observables = this.mangas.map(manga =>
+        this.chapterService.getTotalViewsByMangaId(manga.id_manga)
+      );
+      combineLatest(observables).subscribe(results => {
+        results.forEach((result, index) => {
+          this.mangas[index].totalViews = result.totalViews;
+          console.log(this.mangas[index].totalViews);
+        });
+      });
+    });
   }
 
   onSelectionChange(event: Event): void {
-    const target = event.target as HTMLSelectElement; // Assert type
-    this.selectedValue = target.value; // Access value safely
+    const target = event.target as HTMLSelectElement;
+    this.selectedValue = target.value;
   }
 
   isVisible(option: string): boolean {
     return this.selectedValue === option;
+  }
+
+  viewMangaDetails(id_manga: number) {
+    this.router.navigate(['/titles', id_manga]);
   }
 }
