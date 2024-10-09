b72d64cfff2a5d8962720216a905c4805602a281
nct2k3 <nguyennrdz@gmail.com>
2024-10-10 01:17:17 +0700

    add comment

--- FrontEnd/frontend.client/src/app/view/ViewClient/viewer/viewer.component.ts
+++ FrontEnd/frontend.client/src/app/view/ViewClient/viewer/viewer.component.ts
@@ -3,6 +3,10 @@ import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ChapterService} from '../../../service/Chapter/get_chapter.service';
 import {ChapterDetailsService} from '../../../service/Chapter/chapter_details.service'
+import {CommentService} from "../../../service/Comment/comment.service";
+import {ModelComment} from "../../../Model/ModelComment";
+import {ModelInfoAccount} from "../../../Model/ModelInfoAccoutn";
+import {InfoAccountService} from '../../../service/InfoAccount/info-account.service';
 
 interface Chapter {
   id_chapter: number;
@@ -11,6 +15,20 @@ interface Chapter {
   view: number;
   created_at: Date;
   index: number;
+}
+
+export class CommentData {
+  Comment: ModelComment | null;
+  InfoAccount: ModelInfoAccount | null;
+
+  constructor(
+    comment: ModelComment | null,
+    infoAccount: ModelInfoAccount | null
+  ) {
+    this.Comment = comment;
+    this.InfoAccount = infoAccount;
+
+  }
 }
 
 @Component({
@@ -23,13 +41,24 @@ export class ViewerComponent implements OnInit {
   index!: number;
   images: string[] = [];
   chapters: Chapter[] = [];
+  //nguyen
+  comment: ModelComment[] = [];
+  comments: ModelComment[] = [];
+  listinfoaccount: ModelInfoAccount[] = [];
+
+  listdatacomment: CommentData[] = [];
+  listyourcomment: CommentData[] = [];
+  yourid: number = 1;
+  idchap: number = 1;
 
   constructor(
     private http: HttpClient,
     private route: ActivatedRoute,
     private chapterService: ChapterService,
     private router: Router,
-    private chapterDetailsService: ChapterDetailsService
+    private chapterDetailsService: ChapterDetailsService,
+    private infoAccountservice: InfoAccountService,
+    private commentService: CommentService
   ) {
   }
 
@@ -49,6 +78,14 @@ export class ViewerComponent implements OnInit {
         }
       );
     });
+    this.loadcomment()
+      .then(() => this.loadInfoAccount())
+      .then(() => this.takedata())
+      .catch(error => console.error('Error loading data:', error));
+    this.loadcomment()
+      .then(() => this.loadInfoAccount())
+      .then(() => this.takeyourdata())
+      .catch(error => console.error('Error loading data:', error));
   }
 
   loadImages(): void {
@@ -86,4 +123,78 @@ export class ViewerComponent implements OnInit {
   hasNextChapter(): boolean {
     return this.index < this.chapters.length;
   }
+
+
+  //nguyen
+  takedata() {
+    for (var i = 0; i < this.comments.length; i++) {
+      if (this.comments[i].id_chapter === this.idchap) {
+        for (var j = 0; j < this.listinfoaccount.length; j++) {
+          if (this.comments[i].id_user === this.listinfoaccount[j].id_account && this.comments[i].id_user != this.yourid) {
+            this.listdatacomment.push(new CommentData(
+              this.comments[i],
+              this.listinfoaccount[j]
+            ));
+          }
+        }
+      }
+    }
+    console.log(this.listdatacomment);
+  }
+
+  takeyourdata() {
+    for (var i = 0; i < this.comments.length; i++) {
+      if (this.comments[i].id_chapter === this.idchap) {
+        for (var j = 0; j < this.listinfoaccount.length; j++) {
+          if (this.comments[i].id_user === this.listinfoaccount[j].id_account && this.comments[i].id_user == this.yourid) {
+            this.listyourcomment.push(new CommentData(
+              this.comments[i],
+              this.listinfoaccount[j]
+            ));
+          }
+        }
+      }
+    }
+    console.log(this.listdatacomment);
+  }
+
+  loadcomment(): Promise<void> {
+    return new Promise((resolve, reject) => {
+        this.commentService.getCommnet().subscribe(
+          (data: ModelComment[]) => {
+            this.comments = data;
+            resolve();
+          },
+          error => {
+            console.error('Lỗi ', error);
+          }
+        );
+      }
+    )
+  }
+
+  loadInfoAccount(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.infoAccountservice.getinfoaccount().subscribe(
+        (data: ModelInfoAccount[]) => {
+          this.listinfoaccount = data;
+          resolve();
+        },
+        (error) => {
+          console.error('Error fetching info accounts', error);
+        }
+      );
+    })
+  }
+
+  addComment() {
+    for (let i = 0; i < this.comment.length; i++) {
+      if (this.comment[i].id_chapter == 1) {
+        this.comments.push(this.comments[i])
+      }
+
+    }
+  }
+
+
 }
