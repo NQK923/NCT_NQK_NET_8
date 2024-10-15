97d01463effe7cc058df916ba7a96bc525557993
NQK923 <117274353+NQK923@users.noreply.github.com>
2024-10-15 20:16:48 +0700

    Update Count View for Manga

---
+++ MangaViewHistoryService/Data/MangaViewHistoryDbContext.cs
@@ -0,0 +1,9 @@
+﻿using MangaViewHistoryService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MangaViewHistoryService.Data;
+
+public class MangaViewHistoryDbContext : DbContext
+{
+    public DbSet<MangaViewHistories> MangaViewHistory { get; set; }
+}
---
+++ MangaViewHistoryService/Models/MangaViewHistories.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangaViewHistoryService.Models;
+
+public class MangaViewHistories
+{
+    [Key]
+    public int id_manga { get; set; }
+    public DateTime time { get; set; }
+}
