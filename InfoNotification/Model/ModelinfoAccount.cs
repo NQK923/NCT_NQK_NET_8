e5bfa440df503a96fcbe6151b4deed41f76407eb
NQK923 <117274353+NQK923@users.noreply.github.com>
2024-10-31 07:12:46 +0700

    Delete unused files

--- InfoNotification/Model/ModelinfoAccount.cs
+++
@@ -1,9 +0,0 @@
-namespace Notification.Model;
-
-public class ModelinfoAccount
-{
-    public int id_account { get; set; }
-    public string name { get; set; } // Đặt tên theo chuẩn PascalCase
-    public string email { get; set; }
-    public string cover_img { get; set; }
-}
